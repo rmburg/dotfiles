b4645ecb584e1d366eb243b9fac86ee2b54bdf3c
Narcha <42248344+Narcha@users.noreply.github.com>
2023-05-06 19:36:07 +0200

    Update astronvim config

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -1,68 +1,123 @@
+local function git_co_authors()
+	-- Get all author names and keep unique ones
+	local authors = {}
+	local seen_authors = {}
+	local all_authors = vim.api.nvim_call_function("system", { 'git log --format="%aN <%aE>"' })
+	for _, author in ipairs(vim.split(all_authors, "\n", {})) do
+		if not seen_authors[author] then
+			seen_authors[author] = true
+			authors[#authors + 1] = author
+		end
+	end
+	-- Select co-author using telescope and insert it at current line
+	vim.ui.select(authors, { prompt = "Co-Authors" }, function(author, _)
+		if author then
+			local msg = "Co-authored-by: " .. author
+			local line = vim.api.nvim_win_get_cursor(0)[1]
+			vim.api.nvim_buf_set_lines(0, line, line, true, { msg })
+			vim.api.nvim_win_set_cursor(0, { line + 1, 0 })
+		end
+	end)
+end
+
 local config = {
-  options = {
-    g = {
-      vimtex_view_method = "zathura"
-    },
-    opt = {
-      wrap = true, -- wrap long lines
-      whichwrap = "b,s,<,>,h,l,[,]", -- cursor wrap
-    },
-  },
-  colorscheme = "catppuccin",
-  plugins = {
-    init = {
-      ["catppuccin/nvim"] = {
-        as = "catppuccin",
-        config = function()
-          require("catppuccin").setup({
-            flavour = "macchiato"
-          })
-        end,
-      },
-      ["iamcco/markdown-preview.nvim"] = {
-        run = function()
-          vim.fn["mkdp#util#install"]()
-        end,
-      },
-      ["lervag/vimtex"] = {},
-      ["declancm/cinnamon.nvim"] = {
-        config = function()
-          require("cinnamon").setup()
-        end,
-      },
-      ["simrat39/rust-tools.nvim"] = {}
-    },
-    cmp = {
-      sources = {
-      	{
-          name = "buffer",
-          option = {
-            -- Fix buffer autocompletion treating äöüß as non-word characters
-            keyword_pattern = [[\K\k*]],
-          },
-        },
-      },
-    },
-    heirline = function(config)
-      -- the first element of the default configuration table is the statusline
-      config[1] = {
-        hl = { fg = "fg", bg = "bg" },
-        -- when adding the mode component, enable the mode text with padding to the left/right of it
-        astronvim.status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
-        astronvim.status.component.git_branch(),
-        astronvim.status.component.file_info(),
-        astronvim.status.component.git_diff(),
-        astronvim.status.component.diagnostics(),
-        astronvim.status.component.fill(),
-        astronvim.status.component.macro_recording(),
-        astronvim.status.component.fill(),
-        astronvim.status.component.lsp(),
-        astronvim.status.component.treesitter(),
-        astronvim.status.component.nav(),
-      }
-      return config
-    end,
-  }
+	options = {
+		g = {
+			vimtex_view_method = "zathura",
+		},
+		opt = {
+			wrap = true,                -- wrap long lines
+			whichwrap = "b,s,<,>,h,l,[,]", -- cursor wrap
+		},
+	},
+	colorscheme = "catppuccin",
+	plugins = {
+		{
+			"catppuccin/nvim",
+			as = "catppuccin",
+			config = function()
+				require("catppuccin").setup({
+					flavour = "macchiato",
+				})
+			end,
+		},
+		{
+			"iamcco/markdown-preview.nvim",
+			run = function()
+				vim.fn["mkdp#util#install"]()
+			end,
+		},
+		{ "lervag/vimtex" },
+		{
+			"declancm/cinnamon.nvim",
+			config = function()
+				require("cinnamon").setup()
+			end,
+		},
+		{ "simrat39/rust-tools.nvim" },
+		{
+			"williamboman/mason-lspconfig.nvim",
+			opts = {
+				ensure_installed = { "rust_analyzer", "clangd" },
+			},
+		},
+		{ "p00f/clangd_extensions.nvim" },
+		-- {
+		-- 	"hrsh7th/nvim-cmp",
+		-- 	opts = function(_, opts)
+		-- 		local cmp = require("cmp")
+		-- 		opts.sources = cmp.config.sources({
+		-- 			{
+		-- 				name = "buffer",
+		-- 				option = {
+		-- 					-- Fix buffer autocompletion treating äöüß as non-word characters
+		-- 					keyword_pattern = [[\K\k*]],
+		-- 				},
+		-- 			},
+		-- 		})
+		-- 	end,
+		-- },
+	},
+	lsp = {
+		formatting = {
+			disabled = {
+				"pylsp",
+			},
+		},
+		setup_handlers = {
+			rust_analyzer = function(_, opts)
+				require("rust-tools").setup({ server = opts })
+			end,
+			clangd = function(_, opts)
+				require("clangd_extensions").setup({ server = opts })
+			end,
+		},
+		config = {
+			clangd = {
+				capabilities = {
+					offsetEncoding = "utf-8",
+				},
+			},
+			rust_analyzer = {
+				on_attach = function(_, bufnr)
+					local rt = require("rust-tools")
+					-- Hover actions
+					vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+				end,
+			},
+		},
+	},
+	mappings = {
+		n = {
+			-- Adding git co-authors to commit
+			["<leader>ga"] = {
+				function()
+					git_co_authors()
+				end,
+				desc = "Add git co-authors",
+			},
+		},
+	},
 }
 
 return config
