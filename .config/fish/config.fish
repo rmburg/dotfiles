5a7a62e28b4b8c1640753f3176dea5d92dd110a2
Narcha <42248344+Narcha@users.noreply.github.com>
2024-01-19 00:38:44 +0100

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,19 +16,21 @@ end
 
 # Aliases
 alias ls="ls --color=auto"
-alias ll="exa -la"
+alias ll="eza -la"
+alias llt="eza -laT"
 alias grep="grep --color=auto"
 alias diff="diff --color=auto"
 alias ip="ip -color=auto"
-alias pacman="pacman --color=auto"
-alias grupdate="sudo grub-mkconfig -o /boot/grub/grub.cfg"
 
 # Variables
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
-set fzf_preview_dir_cmd exa --all --color=always
+set fzf_preview_dir_cmd eza --all --color=always
 set -g -x GPG_TTY (tty)
 
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
 
+abbr -a -- h helix
+abbr -a -- m batman
+abbr -a -- grsh git remote show
