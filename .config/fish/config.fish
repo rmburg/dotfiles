30c9b5e30830ea9c75491239b6cc8c03be47b1e3
Narcha <42248344+Narcha@users.noreply.github.com>
2024-04-10 10:58:34 +0200

    Update fish config + stop tracking fish_variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,8 +21,14 @@ abbr -a t tldr
 abbr -a m batman
 
 function update --description 'Update packages from the repos, the AUR and Flatpak'
-    paru -Syu --devel && flatpak update
+    paru -Syu --devel --nocheck && flatpak update
 end
+
+# Expand "...." to "../../../"
+function expand_dots
+    string repeat "../" -n (math (string length $argv[1]) - 1)
+end
+abbr -a dots --position anywhere --regex "\.{3,}" --function expand_dots
 
 # Variables
 fish_add_path ~/.local/bin
@@ -30,6 +36,5 @@ fish_add_path ~/.cargo/bin
 set fzf_preview_dir_cmd exa --all --color=always
 set -g -x GPG_TTY (tty)
 
-string match -q "$TERM_PROGRAM" "vscode"
+string match -q "$TERM_PROGRAM" vscode
 and . (code --locate-shell-integration-path fish)
-
