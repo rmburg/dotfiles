22bc646c197077336f64ea47f7d6ecfee6bb8af6
Narcha <42248344+Narcha@users.noreply.github.com>
2024-04-13 17:20:50 +0200

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 # Launch Hyprland on tty1 after login
-if test "$(tty)" = "/dev/tty1"
+if test "$(tty)" = /dev/tty1
     # exec dbus-launch --exit-with-session Hyprland &> /dev/null
-    exec Hyprland &> /dev/null
+    exec Hyprland &>/dev/null
 end
 
 if status is-interactive
@@ -26,18 +26,25 @@ alias s="kitty +kitten ssh"
 # Variables
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
-fish_add_path ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/
+fish_add_path /usr/lib/rustup/bin
 set fzf_preview_dir_cmd eza --all --color=always
 set -g -x GPG_TTY (tty)
 
 set -x EDITOR hx
 
-string match -q "$TERM_PROGRAM" "vscode"
+string match -q "$TERM_PROGRAM" vscode
 and . (code --locate-shell-integration-path fish)
 
 #Abbreviations
 abbr -a -- m batman
 abbr -a -- t tldr
+abbr -a -- ia ip -br a
+
+# Expand "...." to "../../../"
+function expand_dots
+    string repeat "../" -n (math (string length $argv[1]) - 1)
+end
+abbr -a dots --position anywhere --regex "\.{3,}" --function expand_dots
 
 # Git abbreviations
 abbr -a -- grsh git remote show
