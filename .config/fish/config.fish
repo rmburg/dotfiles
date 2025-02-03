77f1d4aaa24b4eeee9a4def70cf738b59e1a2b8b
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2025-02-03 23:58:02 +0100

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,14 +5,18 @@ if test "$(tty)" = /dev/tty1
 end
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-
-    # Starship prompt
     starship init fish | source
     zoxide init fish | source
 
     bind \b backward-kill-word
 end
+
+# Variables
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
+fish_add_path /usr/lib/rustup/bin
+set fzf_preview_dir_cmd eza --all --color=always
+set -g -x GPG_TTY (tty)
 
 # Aliases
 alias ls="ls --color=auto"
@@ -22,18 +26,8 @@ alias grep="grep --color=auto"
 alias diff="diff --color=auto"
 alias ip="ip -color=auto"
 alias s="kitty +kitten ssh"
+alias pacman="pacman --color=auto"
 alias pluto='julia -e "using Pluto;Pluto.run()"'
-# Variables
-fish_add_path ~/.local/bin
-fish_add_path ~/.cargo/bin
-fish_add_path /usr/lib/rustup/bin
-set fzf_preview_dir_cmd eza --all --color=always
-set -g -x GPG_TTY (tty)
-
-set -x EDITOR hx
-
-string match -q "$TERM_PROGRAM" vscode
-and . (code --locate-shell-integration-path fish)
 
 # Abbreviations
 abbr -a -- m batman
@@ -42,15 +36,16 @@ abbr -a -- ia ip -br a
 abbr -a -- ncu nmcli con up
 abbr -a -- ncd nmcli con down
 
-# Expand "...." to "../../../"
-function expand_dots
-    string repeat "../" -n (math (string length $argv[1]) - 1)
-end
-abbr -a dots --position anywhere --regex "\.{3,}" --function expand_dots
-
 # Git abbreviations
 abbr -a -- glra git pull --rebase --autostash
 abbr -a -- grsh git remote show
 abbr -a -- grsp git restore --patch
 abbr -a -- grstp git restore --staged --patch
 abbr -a -- gstas git stash push --staged
+abbr -a -- gstau git stash push --include-untracked
+
+# Expand "...." to "../../../"
+function expand_dots
+    string repeat "../" -n (math (string length $argv[1]) - 1)
+end
+abbr -a dots --position anywhere --regex "\.{3,}" --function expand_dots
