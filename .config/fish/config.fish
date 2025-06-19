0d9c3277864f10cd3b52a86e14d10222579c1779
rmburg <42248344+rmburg@users.noreply.github.com>
2025-06-19 09:59:32 +0200

    Update fish config to be closer to laptop branch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,15 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-
     starship init fish | source
     zoxide init fish | source
 
     bind \b backward-kill-word
 end
+
+# Variables
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
+set fzf_preview_dir_cmd eza --all --color=always
+set -g -x GPG_TTY (tty)
 
 # Aliases
 alias ls="ls --color=auto"
@@ -14,15 +18,24 @@ alias llt="eza -laT"
 alias grep="grep --color=auto"
 alias diff="diff --color=auto"
 alias ip="ip -color=auto"
+alias s="kitty +kitten ssh"
 alias pacman="pacman --color=auto"
 alias grupdate="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-alias s="kitty +kitten ssh"
 
-abbr -a gstau git stash push -u
-abbr -a gstas git stash push --staged
+# Abbreviations
+abbr -a -- m batman
+abbr -a -- t tldr
+abbr -a -- ia ip -br a
+abbr -a -- ncu nmcli con up
+abbr -a -- ncd nmcli con down
 
-abbr -a t tldr
-abbr -a m batman
+# Git abbreviations
+abbr -a -- glra git pull --rebase --autostash
+abbr -a -- grsh git remote show
+abbr -a -- grsp git restore --patch
+abbr -a -- grstp git restore --staged --patch
+abbr -a -- gstas git stash push --staged
+abbr -a -- gstau git stash push --include-untracked
 
 function update --description 'Update packages from the repos, the AUR and Flatpak'
     paru -Syu --devel --nocheck && flatpak update
@@ -33,12 +46,3 @@ function expand_dots
     string repeat "../" -n (math (string length $argv[1]) - 1)
 end
 abbr -a dots --position anywhere --regex "\.{3,}" --function expand_dots
-
-# Variables
-fish_add_path ~/.local/bin
-fish_add_path ~/.cargo/bin
-set fzf_preview_dir_cmd exa --all --color=always
-set -g -x GPG_TTY (tty)
-
-string match -q "$TERM_PROGRAM" vscode
-and . (code --locate-shell-integration-path fish)
